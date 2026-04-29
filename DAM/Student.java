bc5d9f9573e821303c5ef00bfa6bfe510b39488e
Ariff Izham <ariffizham0307@gmail.com>
2026-04-30 00:50:16 +0800

    added studentType to identify buyer or seller

---
+++ DAM/Student.java
@@ -0,0 +1,95 @@
+public class Student{
+    private String name;
+    private int matricNumber;
+    private int age;
+    private String sex;
+    private String address;
+    private double balance;
+    private String studentType;
+    
+    public Student() {
+        this.name = "no name";
+        this.matricNumber = 0;
+        this.age = 0;
+        this.sex = "no sex";
+        this.address = "no address";
+        this.balance = 0;
+        this.studentType = "no type";
+    }
+
+    public Student(String name, int matricNumber, int age, String sex, String address, float balance) {
+        this.name = name;
+        this.matricNumber = matricNumber;
+        this.age = age;
+        this.sex = sex;
+        this.address = address;
+        this.balance = balance;
+        this.studentType = "no type";
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMatricNumber() {
+        return this.matricNumber;
+    }
+
+    public void setMatricNumber(int matricNumber) {
+        this.matricNumber = matricNumber;
+    }
+    
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return this.sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public String getStudentType() {
+        return this.studentType;
+    }
+
+    public void setStudentType(String studentType) {
+        this.studentType = studentType;
+    }
+
+    public String toString() {
+        return "Name: " + this.name + "\nMatric Number: " 
+                + this.matricNumber + "\nAge: " + this.age 
+                + "\nSex: " + this.sex + "\nAddress: " 
+                + this.address + "\nBalance: RM" + this.balance + "\n";
+    }
+}
+
+// Name: Hamza 
+// Matric Number: 
