1438f0e53385fd1e6e92dead9676b3e45db1cd61
Ariff Izham <ariffizham0307@gmail.com>
2026-04-30 00:47:37 +0800

    still in progress

---
+++ DAM/Order.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Order {
+    private String orderID;
+    private ArrayList <Item> itemList;
+    private boolean isDiscounted;
+    
+    public Order(String orderID) {
+        this.orderID = orderID;
+        this.itemList = new ArrayList<>();
+        this.isDiscounted = false;
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+    
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+    
+    public ArrayList <Item> getItemList() {
+        return itemList;
+    }
+    
+    public void setItemList(ArrayList <Item> itemList) {
+        this.itemList = itemList;
+    }
+    
+    public boolean getIsDiscounted() {
+        return this.isDiscounted;
+    }
+    
+    public void setIsDiscounted(boolean isDiscounted) {
+        this.isDiscounted = isDiscounted;
+    }
+    
+    public void addItem(Item item) {
+        itemList.add(item);
+    }
+    
+    public int getItemCount() {
+        return itemList.size();
+    }
+}
